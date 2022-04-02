c191bf0c8ffbcf25a0c2ee39485986311c1554bf
vbezakova <vendula.bezakova@gmail.com>
2022-04-02 11:41:59 +0200

    Initial commit

---
+++ company_manager/crm/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('company/create', views.CompanyCreateView.as_view(), name='company_create'),
+    path("company/list", views.CourseListView.as_view(), name="company_list")
+]
